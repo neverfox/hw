0c8a41d3fe5b19e64590bf25daffe70988095638
Roman Pearah <github.to.soma@neverbox.com>
2015-09-10 03:49:41 -0400

    Add function for processing a data frame

--- R/hw.R
+++ R/hw.R
@@ -1,4 +1,15 @@
 #' @export
 hw <- function (data, frequency, n) {
-  predict(HoltWinters(ts(data, frequency = frequency)), n.ahead = n)
+  fit <- try(HoltWinters(ts(data, frequency = frequency)), silent = TRUE)
+  if (!inherits(fit, "try-error")) {
+    as.vector(predict(fit, n.ahead = n))
+  } else {
+    rep(NA, n)
+  }
+}
+
+#' @export
+hw_df <- function (df, field, frequency, n) {
+  df$pred <- lapply(df[, field], FUN = hw, frequency = frequency, n = n)
+  df
 }
