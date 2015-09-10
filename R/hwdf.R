c4141c9f037bc6b8f0beb48763a0e57867ad9189
Roman Pearah <github.to.soma@neverbox.com>
2015-09-10 04:06:39 -0400

    Change hw_df to hwdf

--- R/hwdf.R
+++ R/hwdf.R
@@ -1,5 +1,5 @@
 #' @export
-hw_df <- function (df, field, frequency, n) {
+hwdf <- function (df, field, frequency, n) {
   df$pred <- lapply(df[, field], FUN = hw, frequency = frequency, n = n)
   df
 }
